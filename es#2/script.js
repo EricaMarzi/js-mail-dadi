f10c412c9855f75de859c26fa4a247ab9514c3a8
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-21 18:05:14 +0100

    email login

--- es#2/script.js
+++ es#2/script.js
@@ -1,2 +1,33 @@
 console.log("Hi there")
 console.log("General Kenobi")
+
+/*
+- Lista di mail autorizzate nell'array
+- Chiedo all'utente la sua email
+- Se la mail è in lista lo autorizzo con un messaggio in pagina
+*/
+
+// lista email autorizzate
+const emailList = [
+    "pinco@gmail.com" , 
+    "snips@gmail.com" , 
+    "zelda@gmail.com" , 
+    "aloy@gmail.com"
+]
+
+// richiamo gli elementi necessari 
+const userEmail = document.getElementById("user-email")
+const login = document.getElementById("login")
+const loginResult = document.getElementById("login-result")
+
+
+login.addEventListener ("click" , function() { 
+    console.table(emailList)
+
+    let emailValid = emailList.find(email => email === userEmail.value)
+    if (emailValid) {
+        loginResult.innerText = "Login effettuato con successo"
+    } else {
+        loginResult.innerText = "Non sei registrato"
+    }
+})
