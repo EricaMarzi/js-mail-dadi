a7324fba10e84f17317c51c0e26a98a8cbb82ac5
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-21 16:21:21 +0100

    random throw print in page

--- es#1/script.js
+++ es#1/script.js
@@ -1,1 +1,24 @@
 console.log("JS OK")
+
+//? Esercizio 1
+/*
+- Richiamare gli elementi necessari
+- Generare un numero random da 1 a 6 per i due giocatori
+- Confrontare i numeri stabilendo il più alto
+- Stampare in pagina con il risultato
+*/ 
+
+// richiamo gli elementi
+const buttonThrow = document.getElementById("button-throw");
+const winner = document.getElementById("winner");
+const aloyThrow = document.getElementById("aloy");
+const zeldaThrow = document.getElementById("zelda");
+
+// randomizzo da 1 a 6 stampando in pagina
+buttonThrow.addEventListener ("click" , function() {
+    aloyThrow.innerText = "Aloy ha ritato: " + (Math.floor(Math.random() * 6) + 1);
+    zeldaThrow.innerText = "Zelda ha tirato: " + (Math.floor(Math.random() * 6) + 1);
+
+})
+
+
