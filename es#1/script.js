2f5c99752b86ef93837bc01755593ee2cb3ba18f
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-21 16:28:30 +0100

    added if + print in page

--- es#1/script.js
+++ es#1/script.js
@@ -16,9 +16,18 @@ const zeldaThrow = document.getElementById("zelda");
 
 // randomizzo da 1 a 6 stampando in pagina
 buttonThrow.addEventListener ("click" , function() {
-    aloyThrow.innerText = "Aloy ha ritato: " + (Math.floor(Math.random() * 6) + 1);
-    zeldaThrow.innerText = "Zelda ha tirato: " + (Math.floor(Math.random() * 6) + 1);
+    const aloyResult = (Math.floor(Math.random() * 6) + 1);
+    const zeldaResult = (Math.floor(Math.random() * 6) + 1);
+    aloyThrow.innerText = "Aloy ha ritato: " + parseInt(aloyResult);
+    zeldaThrow.innerText = "Zelda ha tirato: " + parseInt(zeldaResult);
 
+    if (aloyResult > zeldaResult) {
+        winner.innerText = "Aloy ha vinto!"
+    } else if (aloyResult < zeldaResult) {
+        winner.innerText = "Zelda ha vinto!"
+    } else {
+        winner.innerText = "Pareggio, ritirate!"
+    }
 })
 
 
